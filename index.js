c328926e9a75899b42d04ebd2cf55ff6003e9bfe
Rexarrascal <serkanozytu@gmail.com>
2022-04-23 19:30:32 +0300

    Transfer port related codes to .bin/www

--- index.js
+++ index.js
@@ -18,6 +18,4 @@ app.use('/drugs', drugsRouter)
 app.use('/orders', ordersRouter)
 app.use('/', indexRouter)
 
-app.listen (3000, () => {
-    console.log('stated listening on 3000')
-})
+module.exports = app
