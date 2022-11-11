301391cd17cd576b6ced299cb1e1704f924354cf
Rexarrascal <serkanozytu@gmail.com>
2022-11-11 21:31:48 +0100

    Delete unnecessary feature

--- backend/routes/me.js
+++
@@ -1,7 +0,0 @@
-// const router = require("express").Router();
-// const customerController = require("../controller/customerController");
-// const auth = require("../config/auth");
-
-// router.get("/", auth, customerController.getCustomerDetails);
-
-// module.exports = router;
